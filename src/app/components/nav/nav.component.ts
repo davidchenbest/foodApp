e9ac6f1d490d9a7d2534f46bc2b5772ae83daf6e
David <david@Unknown-MacBook-Air.local>
2021-07-12 13:22:19 -0400

    nav : numOfItems in cart

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthGuardService } from 'src/app/services/auth-guard.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +9,19 @@ import { AuthGuardService } from 'src/app/services/auth-guard.service';
 })
 export class NavComponent implements OnInit {
   loggedIn: Boolean = false;
+  numOfItems: number = 0;
 
-  constructor(private authService: AuthGuardService) {}
+  constructor(
+    private authService: AuthGuardService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
     this.authService.loggedInSubject.subscribe(
       (data) => (this.loggedIn = data)
+    );
+    this.cartService.itemsSubject.subscribe(
+      (items) => (this.numOfItems = items.length)
     );
   }
 
